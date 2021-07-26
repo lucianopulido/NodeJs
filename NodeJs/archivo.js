64610e25547917b3e35b2234df34fa206afb4f80
lucianopulido <lucianopulido1996@gmail.com>
2021-07-26 15:19:21 -0300

    encabezados, json y objeto response

--- NodeJs/archivo.js
+++ NodeJs/archivo.js
@@ -5,7 +5,8 @@ http.createServer(function (req, res) {
     // creo el servidor
 
     fs.readFile("./index.html", function (err, html) {
-      res.write(html);
-      res.end();
+      res.writeHead(200,{"Content-Type":"application/json"})
+      res.write(JSON.stringify({nombre: "Luciano", username: "LucianoPulido"}));
+      res.end();// cierro la conexion
     }); // leo el archivo html
   }).listen(3000);
