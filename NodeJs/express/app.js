2f3a6ed628f6253a690bf80d34544a312c1677d0
lucianopulido <lucianopulido1996@gmail.com>
2021-08-07 12:04:02 -0300

    motor de vistas jade

--- NodeJs/express/app.js
+++ NodeJs/express/app.js
@@ -2,8 +2,10 @@ var express = require("express");// importo el modulo de express
 
 var app = express(); // creo la app de express 
 
+app.set("view engine", "jade");
+
 app.get("/",function(req,res){
-    res.send("Hola mundo");
+    res.render("index",{hola:"Hola Luciano"});
     res.end();
 });
 
