320d7f6571d71a8733e054973d7ab7e74c21ee22
lucianopulido <lucianopulido1996@gmail.com>
2021-08-15 17:53:14 -0300

    extraccion y parseo de parametros

--- NodeJs/proyecto/app.js
+++ NodeJs/proyecto/app.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var app = express(); 
 
-app.use(express.static('public'));
-app.use(express.json()); // para aplicaciones applitation/json
-app.use(express.urlencoded({extended:true}));
+app.use("/public", express.static('public'));
+
+app.use(express.json()); // para aplicaciones applitation/json // sirve para extraer y parsear datos
+app.use(express.urlencoded({extended:true})); // sirve para extraer y parsear datos
 
 app.set("view engine", "jade");
 
@@ -13,8 +14,13 @@ app.get("/",function(req,res){  // GET
 });
 
 app.get("/login",function(req,res){  // GET
-    console.log(req.body);
     res.render("login"); 
 });
 
+
+app.post("/users", function(req, res){
+    console.log("Contraseña: "+ req.body.password);
+    console.log("Email: "+req.body.email);
+    res.send("Recibimos tus datos");
+});
 app.listen(3000);
