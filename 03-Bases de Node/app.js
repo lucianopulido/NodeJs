6aeeac7c19fe8cfc368cbe72b863d9f7f4fa2fd0
lucianopulido <lucianopulido1996@gmail.com>
2022-02-04 16:19:47 -0300

    se captura parametros desde la consola con yargs

--- 03-Bases de Node/app.js
+++ 03-Bases de Node/app.js
@@ -1,9 +1,14 @@
 const { crearArchivo } = require("./helpers/multiplicar");
+const argv = require("yargs").argv;
 
 console.clear();
-const [, , args3 = "num=5"] = process.argv;
-const [, num] = args3.split("=");
+
+console.log(argv);
+console.log('base:yargs',argv.num);
+
+const num = argv.num;
 
 crearArchivo(num)
   .then((nombreArchivo) => console.log(nombreArchivo, "creado"))
   .catch((err) => console.log(err));
+ 
