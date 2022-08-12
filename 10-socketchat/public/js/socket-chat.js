7b75530d7c5d454a622c40dcf1aeae1829878b7e
lucianopulido <lucianopulido1996@gmail.com>
2022-08-12 18:01:59 -0300

    back-end chat completado

--- 10-socketchat/public/js/socket-chat.js
+++ 10-socketchat/public/js/socket-chat.js
@@ -1,11 +1,27 @@
 var socket = io();
 
-socket.on('connect', function() {
+var params = new URLSearchParams(window.location.search)
+
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html'
+    throw new Error('El nombre y la sala son necesarios')
+}
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+}
+
+socket.on('connect', function () {
     console.log('Conectado al servidor');
+
+    socket.emit('entrarChat', usuario, (resp) => {
+        console.log('Usuarios conectados:', resp)
+    })
 });
 
 // escuchar
-socket.on('disconnect', function() {
+socket.on('disconnect', function () {
 
     console.log('Perdimos conexión con el servidor');
 
@@ -16,13 +32,25 @@ socket.on('disconnect', function() {
 socket.emit('enviarMensaje', {
     usuario: 'Fernando',
     mensaje: 'Hola Mundo'
-}, function(resp) {
+}, function (resp) {
     console.log('respuesta server: ', resp);
 });
 
+/*// Enviar información
+socket.emit('crearMensaje', {
+    mensaje:'hola a todo'
+});*/
+
 // Escuchar información
-socket.on('enviarMensaje', function(mensaje) {
-
+socket.on('crearMensaje', function (mensaje) {
     console.log('Servidor:', mensaje);
-
 });
+
+// Escuchar cuando un usuario entra o sale del chat
+socket.on('listaPersona', (personas) => {
+    console.log(personas);
+});
+
+socket.on('mensajePrivado', (mensaje) => {
+    console.log("Mensaje Privado: ", mensaje)
+})
